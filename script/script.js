1e762997f52e954aabfefae5945b7b9859f49109
lealvesac <lealvesac@gmail.com>
2023-04-18 17:21:59 -0300

    api-async

--- script/script.js
+++ script/script.js
@@ -1,1 +1,7 @@
-console.log("Hello World!")
+async function buscaEndereco() {
+    let consultaCEP = await fetch("https://viacep.com.br/ws/89107000/json/");
+    let consultaCepConvertida = await consultaCEP.json();
+
+    console.log(consultaCepConvertida);
+}
+buscaEndereco();
