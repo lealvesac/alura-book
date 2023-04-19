48f1ca46a16f6ace4990eb10161b31e08aefc19d
lealvesac <lealvesac@gmail.com>
2023-04-19 13:59:03 -0300

    conclusao

--- script/script.js
+++ script/script.js
@@ -1,7 +1,28 @@
-async function buscaEndereco() {
-    let consultaCEP = await fetch("https://viacep.com.br/ws/89107000/json/");
+async function buscaEndereco(cep) {
+    let msgErro = document.getElementById("erro");
+    msgErro.innerHTML = "";
+  try {
+    let consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
     let consultaCepConvertida = await consultaCEP.json();
+    if (consultaCepConvertida.erro) {
+      throw Error("CEP não existente!");
+    }
 
-    console.log(consultaCepConvertida);
+    let cidade = document.getElementById("cidade");
+    let logradouro = document.getElementById("endereco");
+    let estado = document.getElementById("estado");
+    let bairro = document.getElementById("bairro");
+
+    cidade.value = consultaCepConvertida.localidade;
+    logradouro.value = consultaCepConvertida.logradouro;
+    estado.value = consultaCepConvertida.uf;
+    bairro.value = consultaCepConvertida.bairro;
+
+    return consultaCepConvertida;
+  } catch (error) {
+    msgErro.innerHTML = "<p>CEP Inválido, tente novamente!</p>"
+  }
 }
-buscaEndereco();
+
+const cep = document.getElementById("cep");
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
